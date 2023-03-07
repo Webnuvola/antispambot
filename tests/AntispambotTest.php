3ccf2de589d3869abab6bb3e88611a72a7a77acb
Fabio Cagliero <caglierofabio@gmail.com>
2023-03-07 11:09:46 +0100

    Add tests for `antispambot` function

--- tests/AntispambotTest.php
+++ tests/AntispambotTest.php
@@ -5,9 +5,11 @@ namespace Webnuvola\Antispambot\Tests;
 use PHPUnit\Framework\TestCase;
 use Webnuvola\Antispambot\Antispambot;
 
+use function antispambot;
+
 final class AntispambotTest extends TestCase
 {
-    protected static $emails = [
+    protected static array $emails = [
         'info@example.com',
         'dot.in.name@example.com',
         'info@subdomain.example.com',
@@ -15,16 +17,16 @@ final class AntispambotTest extends TestCase
         'another+plus+test@example.com',
     ];
 
-    public function testEmailObfuscation()
+    public function testEmailObfuscation(): void
     {
         foreach (self::$emails as $email) {
             $antispam = Antispambot::antispambot($email);
             $this->assertNotEquals($email, $antispam);
-            $this->assertEquals($email, html_entity_decode($antispam, ENT_HTML5));
+            $this->assertEquals($email, $this->decodeEmail($antispam));
         }
     }
 
-    public function testEmailObfuscationWithHexEncoding()
+    public function testEmailObfuscationWithHexEncoding(): void
     {
         foreach (self::$emails as $email) {
             $antispam = Antispambot::antispambot($email, true);
@@ -32,5 +34,35 @@ final class AntispambotTest extends TestCase
             $antispam = str_replace('+', '&#43;', $antispam);
             $this->assertEquals($email, html_entity_decode(urldecode($antispam), ENT_HTML5));
         }
+    }
+
+    public function testAntispambotFunction(): void
+    {
+        $email = 'demo@example.com';
+
+        $result = antispambot($email);
+        $return = preg_match('/"([^"]+)"/', $result, $matches);
+        $this->assertEquals(1, $return);
+        $this->assertEquals('mailto:' . $email, $this->decodeEmail($matches[1]));
+        $this->assertEquals($email, $this->decodeEmail(strip_tags($result)));
+
+        $result = antispambot($email, 'Contact us');
+        $return = preg_match('/"([^"]+)"/', $result, $matches);
+        $this->assertEquals(1, $return);
+        $this->assertEquals('mailto:' . $email, $this->decodeEmail($matches[1]));
+        $this->assertEquals('Contact us', strip_tags($result));
+
+        $result = antispambot($email, 'Contact us', ['class' => 'text-white', 'target' => '_blank']);
+        $return = preg_match('/"([^"]+)"/', $result, $matches);
+        $this->assertEquals(1, $return);
+        $this->assertEquals('mailto:' . $email, $this->decodeEmail($matches[1]));
+        $this->assertEquals('Contact us', strip_tags($result));
+        $this->assertNotEquals(false, strpos($result, 'class="text-white"'));
+        $this->assertNotEquals(false, strpos($result, 'target="_blank"'));
+    }
+
+    protected function decodeEmail(string $encodedEmail): string
+    {
+        return html_entity_decode($encodedEmail, ENT_HTML5);
     }
 }
